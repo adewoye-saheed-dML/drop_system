bc16f0bbbf48d24857448b1c89f089153b13f66c
adewoye-saheed-dML <adewoyesaheed001@gmail.com>
2026-06-20 18:31:52 +0100

    be

--- src/main.ts
+++ src/main.ts
@@ -12,6 +12,13 @@ async function bootstrap() {
   const app =
     await NestFactory.create(AppModule);
 
+  app.enableCors({
+  origin: [
+    'http://localhost:5173',
+  ],
+  credentials: true,
+});
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
